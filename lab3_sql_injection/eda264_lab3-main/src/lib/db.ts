e8dafcb49fb34aafadfa4a9b6558d77f40b725c9
lyrevik <lyrevik@student.chalmers.se>
2025-02-25 16:53:03 +0100

    something

--- lab3_sql_injection/eda264_lab3-main/src/lib/db.ts
+++ lab3_sql_injection/eda264_lab3-main/src/lib/db.ts
@@ -18,7 +18,7 @@ db.exec(
 		'Html TEXT)'
 );
 
-export function checkUserPass(username: string, password: string): { UID: number, Error?: Error } {
+export function checkUserPass(username: string, password: string): { UID: number; Error?: Error } {
 	const hash = md5(password);
 	const command =
 		"SELECT UID FROM Table_Users WHERE Username='" + username + "' AND Password='" + hash + "'";
@@ -26,17 +26,17 @@ export function checkUserPass(username: string, password: string): { UID: number
 		const uid = db.prepare(command).get() as { UID: number };
 		return uid;
 	} catch (e) {
-		return { UID: 0, Error: e as Error }
+		return { UID: 0, Error: e as Error };
 	}
 }
 
-export function getUserFromSessionID(sid: string): { Username: string, Error?: Error } {
+export function getUserFromSessionID(sid: string): { Username: string; Error?: Error } {
 	const command = "SELECT Username FROM Table_Users WHERE SID='" + sid + "'";
 	try {
 		const username = db.prepare(command).get() as { Username: string };
 		return username;
 	} catch (e) {
-		return { Username: "", Error: e as Error };
+		return { Username: '', Error: e as Error };
 	}
 }
 
@@ -55,29 +55,20 @@ export function addUserSessionID(uid: number, sid: string): boolean {
 		return false;
 	}
 }
-
-export function addComment(sid: string, comment: string): { done: boolean, Error?: Error } {
+// SANITIZED FUNCTION RIGHT HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export function addComment(sid: string, comment: string): { done: boolean; Error?: Error } {
 	const uid = getUIDFromSessionID(sid);
 	const day = new Date();
-	const command =
-		"INSERT INTO Table_Comments (Timestamp, User, Html) Values('" +
-		day.toDateString() +
-		"', '" +
-		uid +
-		"', '" +
-		comment +
-		"')";
+	const stmt = db.prepare('INSERT INTO Table_Comments (Timestamp, User, Html) VALUES( ?, ?, ?)');
 	try {
-		db.exec(command);
+		stmt.run(day.toDateString(), uid, comment);
 		return { done: true };
 	} catch (e) {
 		return { done: false, Error: e as Error };
 	}
 }
 
-export function getComments(): [
-	{ Timestamp: string; Username: string; Html: string }
-] {
+export function getComments(): [{ Timestamp: string; Username: string; Html: string }] {
 	const command =
 		'SELECT Timestamp, Username, Html FROM ' +
 		'Table_Comments INNER JOIN Table_Users ON ' +
